6be78ccdb7ab749cdd552d1e69b8e4e081b02fae
Dany Corral <danycorral17@gmail.com>
2021-02-10 12:13:53 -0600

    Defining all the groups

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ uint8 sonbitpares(uint16 word);
 
 int main()
 {
-    printf("Hello World, I'm Dany!\n");
+    printf("Hello World, we're Olaf and Dany!\n");
     printf("%d\n",variableentrada);
     printf("%d\n",createhammingcode(0));
 
@@ -34,6 +34,9 @@ uint16 createhammingcode(uint16 word){
     uint16 grupo3 = 0;
     uint16 grupo4 = 0;
     grupo1 = (word &0x6D5);
+    grupo2 = (word &0x5B3);
+    grupo3 = (word &0x38F);
+    grupo4 = (word &0x07F);
     codigosecuridad = (sonbitpares(grupo1)<<11) + (sonbitpares(grupo2)<<12) +(sonbitpares(grupo3)<<13) + (sonbitpares(grupo4)<<14) ;
     resultado = word + codigosecuridad;
     resultado+= (sonbitpares(resultado)<<15);
